90bce4161196e2d592357225558c6e272e34cfd1
abelko <esung1129@gmail.com>
2017-11-29 12:39:41 +0900

    Add hashtags

--- feed/models.py
+++ feed/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here. #model을 만들고 나면, 항상 makemigrations을 해주어야 한다.
 #makemigrations는 파일을 만들어 준다, 수정된 내용을 기록해 준다.
 #migrate는 기록된 내용을 바탕으로 데이터베이스 테이블을 만들고, 컬럼을 없애는 등 세부적인 내용을 변경하는 작업을 해준다.
+class HashTag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Article(models.Model):
     DEVELOPMENT = "dv"
     PERSONAL = "ps"
@@ -19,6 +25,8 @@ class Article(models.Model):
         default=DEVELOPMENT,
     )
 
+    hashtag = models.ManyToManyField(HashTag)
+
     def __str__(self):
         return self.title
 
@@ -29,9 +37,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return "{}에 댓글 : {}".format(self.article.title, self.content)
-
-class HashTag(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
